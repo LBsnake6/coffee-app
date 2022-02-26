546c1274ae81a75c1bc55744be799feef6aa58ce
Ming Creekmore <mec5765@g.rit.edu>
2022-02-26 16:59:39 -0500

    Updated partner information

--- coffee-app/src/app/data/mock_partners.ts
+++ coffee-app/src/app/data/mock_partners.ts
@@ -2,11 +2,25 @@ import { Partners } from "../interfaces/partners";
 
 export const PARTNERS: Partners[] = [
 
-    {name: "a", location: "rochester,ny", website: "a.com", hours:"11",items:""},
-    {name: "b", location: "rochester,ny", website: "b.com", hours:"11",items:""},
-    {name: "c", location: "rochester,ny", website: "c.com", hours:"11",items:""},
-    {name: "d", location: "rochester,ny", website: "d.com", hours:"11",items:""},
-    {name: "e", location: "rochester,ny", website: "e.com", hours:"11",items:""},
-    {name: "f", location: "rochester,ny", website: "f.com", hours:"11",items:""},
-    {name: "g", location: "rochester,ny", website: "g.com", hours:"11",items:""}
+    {name: "Java's Cafe", location: "16 Gibbs St\nRochester, NY 14604", website: "javascafe.com",
+     hours: "Sunday-Saturday\t7AM-Midnight", items:"", vibe: "Hip/Indie"},
+    {name: "Boulder Coffee Co Cafe and Lounge", location: "100 Alexander St\nRochester, NY 14620", 
+     website: "bouldercoffeeroaster.com", hours: "Saturday & Sunday\t8AM-8PM\nMonday-Friday\t7AM-8PM",
+     items:"", vibe: "Hip and Funky"},
+    {name: "Village Bakery & Cafe", location: "145 Culver RD\nRochester, NY 14620", website: "villagebakeries.com/#",
+     hours: "Sunday-Saturday\t6:30AM-3PM",items:"", vibe: "Modern/Farmhouse"},
+    {name: "Caramel Bakery & Bar", location: "647 Park Ave\nRochester, NY 14607", website: "caramelbakeryandbar.com",
+     hours:"Sunday\t10AM-2PM\nMonday & Tuesday\tClosed\nWednesday-Saturday\t5PM-11PM", items:"",
+     vibe: "Romantic and Relaxed"},
+    {name: "Fuego Coffee Roasters", location: "1 Woodbury Blvd\nRochester, NY 14604", website: "fuegocoffee.com",
+     hours: "Sunday-Saturday\t7AM-5PM", items: "", vibe: "Low-key"},
+    {name: "Melo Coffee & Kitchen", location: "696 University Ave\nRochester, NY 14607",
+     website: "melocoffeeandkitchen.com", hours: "Sunday\t8AM-2PM\nMonday-Saturday\t8AM-6PM", items:"",
+     vibe: "Welcoming/Relaxing"},
+    {name: "Ugly Duck Coffee", location: "89 Charlotte St\nRochester, NY 14607", website: "uglyduckcoffee.com",
+     hours:"Sunday & Saturday\t8AM-5PM\nMonday-Friday\t7AM-5PM", items: "", vibe: "Trendy"},
+    {name: "Founders Cafe", location: "13 S Fitzhugh St\nRochester, NY 14614", website: "founderscaferochester.com",
+     hours: "Sunday\tClosed\nMonday-Friday\t7AM-5PM\nSaturday\t9AM-3PM", items: "", vibe: "Modest Cafe"},
+    {name: "Equal Grounds", location: "750 South Ave\nRochester, NY 14620", website: "equalgrounds.com", 
+     hours: "Sunday & Saturday\t9AM-10PM\nMonday-Friday\t8AM-10PM", items: "", vibe: "Cool Hangout"}
 ]
