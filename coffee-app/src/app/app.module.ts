deb6de3b06af84745a5b86913aec5abe7518ccf7
lb5562 <lucille_blain@live.com>
2022-02-26 13:43:55 -0500

    added components for main pages

--- coffee-app/src/app/app.module.ts
+++ coffee-app/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PartnersComponent } from './partners/partners.component';
+import { AboutComponent } from './about/about.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    PartnersComponent,
+    AboutComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule,
